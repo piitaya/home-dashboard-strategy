ac2fdf59fc4dd07c81032a6394e6779f943f5dd0
Paul Bottein <paul.bottein@gmail.com>
2024-10-03 17:38:43 +0200

    Add sections

---
+++ src/helpers/entity.ts
@@ -0,0 +1,49 @@
+import { HassEntity } from "home-assistant-js-websocket";
+import { ensureArray } from "../ha/common/array/ensure-array";
+import { computeDomain } from "../ha/common/entity";
+import { HomeAssistant } from "../ha/types";
+
+export const getEntityArea = (hass: HomeAssistant, entityId: string) => {
+  const entity = hass.entities[entityId];
+  if (!entity) {
+    return null;
+  }
+  if (entity.area_id) {
+    return entity.area_id;
+  }
+  const device = entity.device_id ? hass.devices[entity.device_id] : null;
+  if (device && device.area_id) {
+    return device.area_id;
+  }
+  return null;
+};
+
+type EntityFilter = {
+  domain?: string | string[];
+  device_class?: string | string[];
+};
+
+export const entityFilter = (hass: HomeAssistant, filter: EntityFilter) => {
+  const domains = ensureArray(filter.domain) ?? [];
+  const deviceClasses = ensureArray(filter.device_class) ?? [];
+  return (entityId: string) => {
+    if (domains.length > 0) {
+      const domain = computeDomain(entityId);
+      if (!domains.includes(domain)) {
+        return false;
+      }
+    }
+    if (deviceClasses.length > 0) {
+      const stateObj = hass.states[entityId] as HassEntity | undefined;
+      const dc = stateObj?.attributes.device_class;
+      if (!dc) {
+        return false;
+      }
+      const deviceClasses = ensureArray(filter.device_class);
+      if (!deviceClasses.includes(dc)) {
+        return false;
+      }
+    }
+    return true;
+  };
+};
--- src/stategies/view/area-view-strategy.ts
+++ src/stategies/view/area-view-strategy.ts
@@ -1,7 +1,10 @@
 import { ReactiveElement } from "lit";
 import { customElement } from "lit/decorators.js";
+import { LovelaceBadgeConfig } from "../../ha/data/lovelace/config/badge";
+import { LovelaceSectionConfig } from "../../ha/data/lovelace/config/section";
 import { LovelaceViewConfig } from "../../ha/data/lovelace/config/view";
 import { HomeAssistant } from "../../ha/types";
+import { entityFilter, getEntityArea } from "../../helpers/entity";
 
 export type AreaViewStrategyConfig = {
   area?: string;
@@ -23,19 +26,171 @@ export class AreaViewStrategy extends ReactiveElement {
       throw new Error("Unknown area");
     }
 
+    const sections: LovelaceSectionConfig[] = [];
+
+    const entityIds = Object.keys(hass.states).filter((entityId) => {
+      const areaId = getEntityArea(hass, entityId);
+      const entity = hass.entities[entityId];
+      if (entity?.entity_category || entity?.hidden) {
+        return false;
+      }
+      return areaId === config.area;
+    });
+
+    const lightIds = entityIds.filter(entityFilter(hass, { domain: "light" }));
+
+    if (lightIds.length > 0) {
+      sections.push({
+        type: "grid",
+        cards: [
+          {
+            type: "heading",
+            heading: "Lights",
+            icon: "mdi:lamps",
+          },
+          ...lightIds.map((entityId) => ({
+            type: "tile",
+            entity: entityId,
+          })),
+        ],
+      });
+    }
+
+    const coverIds = entityIds.filter(entityFilter(hass, { domain: "cover" }));
+
+    if (coverIds.length > 0) {
+      sections.push({
+        type: "grid",
+        cards: [
+          {
+            type: "heading",
+            heading: "Cover",
+            icon: "mdi:window-shutter",
+          },
+          ...coverIds.map((entityId) => ({
+            type: "tile",
+            entity: entityId,
+          })),
+        ],
+      });
+    }
+
+    const badges: LovelaceBadgeConfig[] = [];
+
+    const temperatureSensorIds = entityIds.filter(
+      entityFilter(hass, {
+        domain: "sensor",
+        device_class: "temperature",
+      })
+    );
+
+    if (temperatureSensorIds.length > 0) {
+      badges.push(
+        ...temperatureSensorIds.map((entityId) => ({
+          type: "entity",
+          entity: entityId,
+          color: "red",
+        }))
+      );
+    }
+
+    const humiditySensorIds = entityIds.filter(
+      entityFilter(hass, {
+        domain: "sensor",
+        device_class: "humidity",
+      })
+    );
+
+    if (humiditySensorIds.length > 0) {
+      badges.push(
+        ...humiditySensorIds.map((entityId) => ({
+          type: "entity",
+          entity: entityId,
+          color: "purple",
+        }))
+      );
+    }
+
+    const mediaPlayerIds = entityIds.filter(
+      entityFilter(hass, {
+        domain: "media_player",
+      })
+    );
+
+    if (mediaPlayerIds.length > 0) {
+      sections.push({
+        type: "grid",
+        cards: [
+          {
+            type: "heading",
+            heading: "Media players",
+            icon: "mdi:multimedia",
+          },
+          ...mediaPlayerIds.map((entityId) => ({
+            type: "tile",
+            entity: entityId,
+            show_entity_picture: true,
+          })),
+        ],
+      });
+    }
+
+    const securityIds = entityIds.filter(
+      entityFilter(hass, {
+        domain: ["lock", "alarm_control_panel"],
+      })
+    );
+
+    if (securityIds.length > 0) {
+      sections.push({
+        type: "grid",
+        cards: [
+          {
+            type: "heading",
+            heading: "Security",
+            icon: "mdi:shield",
+          },
+          ...securityIds.map((entityId) => ({
+            type: "tile",
+            entity: entityId,
+          })),
+        ],
+      });
+    }
+
+    const otherIds = entityIds.filter((entityId) => {
+      return (
+        !lightIds.includes(entityId) &&
+        !coverIds.includes(entityId) &&
+        !temperatureSensorIds.includes(entityId) &&
+        !humiditySensorIds.includes(entityId) &&
+        !mediaPlayerIds.includes(entityId) &&
+        !securityIds.includes(entityId)
+      );
+    });
+
+    if (otherIds.length > 0) {
+      sections.push({
+        type: "grid",
+        column_span: 4,
+        cards: [
+          {
+            type: "heading",
+            heading: "Other",
+            icon: "mdi:file",
+          },
+          ...otherIds.map((entityId) => ({
+            type: "tile",
+            entity: entityId,
+          })),
+        ],
+      });
+    }
+
     return {
       type: "sections",
-      sections: [
-        {
-          type: "grid",
-          cards: [
-            {
-              type: "area",
-              area: area.area_id,
-            },
-          ],
-        },
-      ],
+      badges: badges,
+      sections: sections,
     };
   }
 }
